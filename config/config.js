418e964c8e5ff60051beb91dd641a287c320ea03
帅纯亮 <shuaichunliang@heqiauto.com>
2018-01-10 21:40:02 +0800

    buddy and group msg listener,smartqq can only send text msg

---
+++ config/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by purelightme on 2018/1/10.
+ */
+
+let config = {
+    qqNumber:1841675256,
+    groupName:'爱比价',
+};
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 /**
  * Created by purelightme on 2018/1/9.
  */
-const { QQ } = require('qq-bot-rebown');
+const {QQ} = require('qq-bot-rebown');
+const config = require('./config/config');
 
-const qq = new QQ();
+const qq = new QQ({cookiePath: './qqbot.cookie'});
+
+let self;
+
+qq.on('login-success', () => {
+    qq.getSelfInfo()
+        .then((info) => {
+            console.log("自己的信息：" + JSON.stringify(info));
+            self = info;
+        }, (err) => {
+            console.log("出错了：" + err);
+        })
+});
 
 // 设置 “收到消息” 事件监听
 qq.on('msg', (msg) => {
-    console.log(JSON.stringify(msg));
+
 });
 
 // 设置 “收到好友消息” 事件监听
 qq.on('buddy', (msg) => {
-    qq.sendBuddyMsg(msg.id, `Hello, ${msg.name}`);
+    qq.sendBuddyMsg(msg.id, `${msg.name}：${msg.content}`);
 });
 
-qq.on('group',(msg) => {
-
+qq.on('group', (msg) => {
+    if (msg.groupName != config.groupName) {
+        return;
+    }
+    if (msg.id == config.qqNumber) {
+        return;
+    }
+    qq.sendGroupMsg(msg.groupId, `${msg.name}：${msg.content}`)
 });
 
 // 不要忘记启动 :)
